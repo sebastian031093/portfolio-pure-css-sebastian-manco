30d22b2b7bfe41725934a103c4002c95b37e6671
Sebasitan Manco Gomez <sebasmg1393@gmail.com>
2021-06-29 23:18:32 -0500

    portfolio init

--- js/app.js
+++ js/app.js
@@ -22,7 +22,7 @@ if(navClose){
 }
 
 // REMOVE MENU MOBILE
-
+//TODO:AQUI PUEDES DELEGAR EVENTOS
 const navLink = document.querySelectorAll(".nav__link");
 
 function linkAction() {
@@ -31,3 +31,73 @@ function linkAction() {
 }
 
 navLink.forEach(elements => elements.addEventListener('click',linkAction))
+
+
+/////////////////// ACCORDION SKILLS/////////////////////
+//TODO:AQUI PUEDES DELEGAR EVENTOS
+const skillsContent = document.getElementsByClassName("skills__content"),
+  skillsHeader = document.querySelectorAll(".skills__header");
+
+function toggleSkills() {
+    console.log('hola me diste clik');
+    console.log(this.parentNode.className);
+    let itemClass = this.parentNode.className;
+
+    for (let index = 0; index < skillsContent.length; index++) {
+        skillsContent[index].className = "skills__content skills__close";
+    }
+
+    if(itemClass === "skills__content skills__close"){
+        this.parentNode.className = "skills__content skills__open";
+    }
+}
+
+skillsHeader.forEach((elemtn) =>{
+    elemtn.addEventListener('click', toggleSkills)
+})
+
+///////////////////////////////////////////////////////////////////////QUALIFICATION TABS.
+
+const tabs = document.querySelectorAll('[data-target]'),
+    tabContents = document.querySelectorAll('[data-content]')
+
+tabs.forEach( tab => {
+    tab.addEventListener('click', () =>{
+        const target = document.querySelector(tab.dataset.target)
+
+        tabContents.forEach(tabContent => {
+            tabContent.classList.remove('qualification__active')
+        });
+        target.classList.add("qualification__active");
+
+        tabs.forEach(tab =>{
+            tab.classList.remove("qualification__active");
+        });
+
+        tab.classList.add("qualification__active");
+    });
+});
+
+///////////////////// SERVICES MODAL.
+
+const modalViews = document.querySelectorAll('.services__modal'),
+    modalBtns = document.querySelectorAll('.services__button'),
+    modalClose = document.querySelectorAll('.services__modal-close')
+
+let modal = function (modalClick) {
+    modalViews[modalClick].classList.add('active-modal')
+}
+
+modalBtns.forEach((modalBtn, i) => {
+    modalBtn.addEventListener('click', () =>{
+        modal(i)
+    })
+});
+
+modalClose.forEach((modalClose) =>{
+    modalClose.addEventListener('click', () =>{
+        modalViews.forEach((modalView) =>{
+            modalView.classList.remove('active-modal')
+        })
+    })
+})
